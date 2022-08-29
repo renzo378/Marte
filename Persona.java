a211243c832715b2b58958a75f7d244b550d9f25
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-08-29 17:29:49 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,8 +1,14 @@
 public class Persona{
 public int x;
+public String y;
 public void algo(int x,int y){
 this.x = x +1+x;
   x = x+y;
 }
-  public void Personaa(){}
+ public String Personaa(){
+  return y + "hola";
+ }
+ public void nose(int l){
+   l = l +5;
+ }
 }
