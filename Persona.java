098f77fc67a47fe92239d582eb342afe66ea730f
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-08-30 20:30:28 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,20 +1,5 @@
-public class Auto {
-    private int x;
-    public boolean y;
-    public int h;
-    public int b;
-    private int k;
-    public void algo(int x,int b){
-        int y = 0;
-        int h = 5;
-        x = x+3+b;
-        k = x+b;
-        this.y = true;     
-    }
-    public void nose(){
-    
-    }
-    public void algomas(){
-     k = k+2;   
-    }
+public interface Auto {
+ public static final String EMPRESA = "Patito, S. A.";
+ public static final int EMPRESA = "Patito, S. A.";
+ public void moverPosicion (int x, int y);
 }
