8bd8d8cdaf8442873f3267e99d2edc2239a83de9
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-08-30 12:31:46 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,8 +1,12 @@
 public class Auto {
     public int x;
     public boolean y;
-    public void algo(int x){
+    public void algo(int x,int b){
         int y = 0;
+        int h = 0;
+        x = x+3+b;
         this.x = y;
+        this.y = true;
+        
     }
 }
