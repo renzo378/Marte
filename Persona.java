b7dfa832e5cf8344c3050a1f8ebfea53bc91492a
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-08-30 20:34:43 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,5 +1,5 @@
 public interface Auto {
  public static final String EMPRESA = "Patito, S. A.";
- public static final int EMPRESA = "Patito, S. A.";
+ public static final int EMPRESAA = 5;
  public void moverPosicion (int x, int y);
 }
