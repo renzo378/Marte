3eb4cc41b0432925fbf7bc3da45b025f24f628d9
renzo378 <69973902+renzo378@users.noreply.github.com>
2022-08-31 18:16:50 -0300

    Update Camionnn.java

--- Camionnn.java
+++ Camionnn.java
@@ -2,12 +2,22 @@ public class Camion{
 public boolean x = true;
 public boolean y = false;
 public void Algo(){
+  int x = (l>k)? 5 : 4;
   if(x && y){
-    
+    if(x||y){}
   }else if(y){
     
   }else{
   }
+  switch (day) 
+        {
+            case 1:  dayString = "Lunes";
+                    break;
+    case 2 : dayString = "Martes";
+      break;
+            default: dayString = "Dia inválido";
+                     break;
+                     }
 }
 
 
